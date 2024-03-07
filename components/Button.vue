1f3837458d021bf7479bd68046f488ff1febfcf3
andreasrein <reinholdsson.andreas@gmail.com>
2024-03-07 16:27:57 +0100

    Add about page and button component

---
+++ components/Button.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    title?: string,
+    path?: string,
+  }>()
+  const btnClasses = "transition-all inline-block bg-white border-none rounded-lg text-black mix-blend-screen py-2 px-4 font-bold hover:scale-105 hover:bg-rose-200"
+</script>
+<template>
+  <NuxtLink v-if="path" :to={path} :class="btnClasses">
+    <slot></slot>
+  </NuxtLink>
+  <button v-else :class="btnClasses">
+    <slot></slot>
+  </button>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,22 @@
 <template>
-  about
+  <section class="flex flex-col justify-center p-8 max-w-[1400px] min-[1400px]:mx-auto">
+    <h1 class="text-4xl sm:text-7xl font-bold text-white mb-8">This is me...</h1>
+    <div class="w-9/12 text-white text-lg mb-8">
+      <p class="mb-4">
+        I am a curious and driven developer who is constantly seeking new challenges. It all started with a small high school course. It was at this time I realized that you could combine color and shape with technology in a creative way.
+I have long been interested in music and art, but it wasn't until now that I discovered the magic that could be created on a computer screen using HTML and CSS. 
+      </p>
+      <p class="mb-4">
+        I followed that path, and today I call myself a Frontend/Fullstack developer. I studied at Mediainstitutet in Stockholm where I learned the basics of object-oriented programming. I learned about databases, UX, and delved into JavaScript and its plethora of frameworks. This later led me to an internship at a consulting company where I worked on real in-house projects. 
+      </p>
+      <p>
+        With my previous aesthetic interests, I feel like a strong frontend developer with an eye and a feel for details. This greatly facilitates my work as I now closely collaborate with designers.
+      </p>
+    </div>
+    <div>
+      <Button path="/experience">
+        Check out what i've done!
+      </Button>
+    </div>  
+  </section>
 </template>

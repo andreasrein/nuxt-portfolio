65b069cc7dd26cfe576ce29f554e91ed8dfd0e2c
andreasrein <reinholdsson.andreas@gmail.com>
2024-03-08 14:35:40 +0100

    Add favicon and page title

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: 'Andreas Reinholdsson',
+  meta: [
+    { name: 'Andreas Reinholdssons portfolio', content: 'Me and my work.' }
+  ]
+})
+</script>
+
 <template>
   <div class="relative flex flex-col box-border bg-gradient-to-r from-indigo-500 to-emerald-500 h-full min-h-screen w-full text-white pb-8">
     <Navigation />

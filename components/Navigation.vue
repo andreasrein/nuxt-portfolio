b5e0d3b52fe3b942a43873271a11deed626acbda
andreasrein <reinholdsson.andreas@gmail.com>
2024-04-23 16:09:09 +0200

    Update navigation and layout content margins

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -32,20 +32,20 @@
     {path: '/about', title: 'ABOUT'},
     {path: '/experience', title: 'EXPERIENCE'}
   ]
-  const spanClasses = 'block absolute h-[9px] w-full bg-white rounded transition-all'
+  const spanClasses = 'block absolute h-[6px] w-full bg-white rounded transition-all'
 </script>
 <template>
   <nav ref="navbar" class="z-10 right-0 flex flex-col items-end">
     <div class="right-0 top-0 z-10">
-      <button class="p-8" @click="handleExpanderClick">
-        <div class="w-[60px] h-[45px] relative transition">
-          <span class="origin-top-left" :class="spanClasses, {'rotate-45 -top-0.5 left-3': isOpen, 'rotate-0 top-0 left-0': !isOpen}"></span>
-          <span class="origin-left top-[18px] left-0" :class="spanClasses, {'opacity-0 w-0': isOpen, 'opacity-100': !isOpen}"></span>
-          <span class="origin-bottom-left bottom-0" :class="spanClasses, {'-rotate-45 left-3 -bottom-0.5': isOpen, 'rotate-0 left-0': !isOpen}"></span>
+      <button class="p-8 transition-colors hover:bg-white/10" @click="handleExpanderClick">
+        <div class="w-12 h-9 relative transition">
+          <span class="origin-top-left" :class="spanClasses, {'rotate-45 -top-[1px] left-[8px]': isOpen, 'rotate-0 top-0 left-0': !isOpen}"></span>
+          <span class="origin-left top-[15px] left-0" :class="spanClasses, {'opacity-0 w-0': isOpen, 'opacity-100': !isOpen}"></span>
+          <span class="origin-bottom-left bottom-[-1px]" :class="spanClasses, {'-rotate-45 left-[8px] -bottom-[1px]': isOpen, 'rotate-0 left-0': !isOpen}"></span>
         </div>
       </button>
     </div>
-    <div v-if="isOpen" class="fixed text-2xl mt-28 z-10 py-8 px-16 h-full bg-white/20 backdrop-blur transition-all">
+    <div v-if="isOpen" class="fixed text-2xl mt-24 z-10 py-8 px-16 h-full bg-white/20 backdrop-blur transition-all">
       <ul class="flex flex-col sm:flex-col gap-4">
         <li v-for="link in links" :key="link.path" class="mb-4">
           <NuxtLink
--- components/layout/Section.vue
+++ components/layout/Section.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex flex-col px-4 sm:px-8 max-w-[1400px] min-[1400px]:mx-auto">
+  <section class="flex flex-col px-4 mb-20 sm:px-8 max-w-[1400px] min-[1400px]:mx-auto">
     <slot></slot>
   </section>
 </template>

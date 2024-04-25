48b11dae69c052fe585f2db0308d5f176e824a1f
andreasrein <reinholdsson.andreas@gmail.com>
2024-04-25 16:37:07 +0200

    Add form validation

--- components/ContactModal.vue
+++ components/ContactModal.vue
@@ -5,6 +5,12 @@ const showForm = ref(false)
 const name = ref('')
 const email = ref('')
 const message = ref('')
+const formSubmitted = ref(false)
+const isValid = reactive({
+  name: false,
+  email: false,
+  message: false
+})
 
 const handleContactClick = () => {
   showForm.value = !showForm.value 
@@ -15,7 +21,22 @@ const setFormStatus = (val: 'unsent' | 'pending' | 'success' | 'error') => {
   formStatus.value = val
 }
 
+const validateForm = () => {
+  !formSubmitted.value ? formSubmitted.value = true : null
+
+  name.value.length !== 0 ? isValid.name = true : isValid.name = false
+  email.value.length !== 0 && email.value.indexOf('@') > -1 ? isValid.email = true : isValid.email = false
+  message.value.length > 6 ? isValid.message = true : isValid.message = false
+
+  const hasError = Object.values(isValid).some(val => val === false);
+
+  return hasError
+}
+
 async function handleFormSubmit(event: Event) {
+  const hasError = validateForm()
+  if (hasError) return
+
   setFormStatus('pending')
   try {
     const body = new FormData(event.target as HTMLFormElement)
@@ -57,11 +78,11 @@ async function handleFormSubmit(event: Event) {
           </button>
         </div>
         <div class="bg-emerald-500/10 text-emerald-500 rounded p-4 sm:p-8 mb-8">
-          <div class="flex gap-2 items-center mb-2">
+          <div class="flex gap-2 items-center mb-2 hover:underline">
             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" class="fill-emerald-500"><path d="M280-40q-33 0-56.5-23.5T200-120v-720q0-33 23.5-56.5T280-920h400q33 0 56.5 23.5T760-840v720q0 33-23.5 56.5T680-40H280Zm0-200h400v-480H280v480Z"/></svg>
             <a href="tel:+46768619378">+46 (0)76 861 93 78</a>
           </div>
-          <div class="flex gap-2 items-center">
+          <div class="flex gap-2 items-center transition-all hover:underline">
             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" class="fill-emerald-500"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280 320-200v-80L480-520 160-720v80l320 200Z"/></svg>
             <a href="mailto:reinholdsson.andreas@gmail.com">reinholdsson.andreas@gmail.com</a>
           </div>
@@ -76,24 +97,48 @@ async function handleFormSubmit(event: Event) {
           <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" class="fill-emerald-500 h-10 w-10"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z"/></svg>
           <div class="ml-2">Sent!</div>
         </div>
-        <form v-if="formStatus !== 'pending' && formStatus !== 'success'" class="flex flex-col text-emerald-500" id="my-form" @submit.prevent="handleFormSubmit">
-          <div class="flex flex-col mb-4 font-roboto text-md">
+        <form v-if="formStatus !== 'pending' && formStatus !== 'success'" novalidate class="flex flex-col text-emerald-500" id="my-form" @submit.prevent="handleFormSubmit">
+          <div class="flex flex-col mb-6 font-roboto text-md relative">
             <label for="name" class="mb-2">
-              Name
+              Full Name
             </label>
-            <input class="rounded p-2 text-black bg-white/15 border-2 border-emerald-500 shadow-inner" id="name" type="name" name="name" v-model="name" />
+            <input
+              class="rounded p-2 text-black bg-white/15 border-2 border-emerald-500 shadow-inner"
+              :class="{'border-red-500': formSubmitted && !isValid.name}"
+              id="name"
+              type="name"
+              name="name"
+              v-model="name" />
+            <span v-if="formSubmitted && !isValid.name" class="absolute text-red-500 text-xs -bottom-5">Please enter your full name</span>
+
           </div>
-          <div class="flex flex-col mb-4 font-roboto text-md">
+          <div class="flex flex-col mb-6 font-roboto text-md relative">
             <label for="email" class="mb-2">
               Email
             </label>
-            <input class="rounded p-2 text-black bg-white/15 border-2 border-emerald-500 shadow-inner" id="email" type="email" name="email" v-model="email" />
+            <input 
+              class="rounded p-2 text-black bg-white/15 border-2 border-emerald-500 shadow-inner"
+              :class="{'border-red-500': formSubmitted && !isValid.email}"
+              id="email"
+              type="email"
+              name="email"
+              v-model="email" />
+            <span v-if="formSubmitted && !isValid.email" class="absolute text-red-500 text-xs -bottom-5">Please enter a valid email address</span>
+
           </div>
-          <div class="flex flex-col mb-8 font-roboto text-md">
+          <div class="flex flex-col mb-8 font-roboto text-md relative">
             <label for="message" class="mb-2">
               Message
             </label>
-            <textarea class="rounded p-2 text-black bg-white/15 border-2 border-emerald-500 shadow-inner" id="message" type="text" name="message" v-model="message" rows="5" />
+            <textarea
+              class="rounded p-2 text-black bg-white/15 border-2 border-emerald-500 shadow-inner"
+              :class="{'border-red-500': formSubmitted && !isValid.message}"
+              id="message"
+              type="text"
+              name="message"
+              v-model="message"
+              rows="5" />
+            <span v-if="formSubmitted && !isValid.message" class="absolute text-red-500 text-xs -bottom-5">Please enter a message</span>
           </div>
           <div class="z-10 text-right">
             <Button type="submit" inverted>Submit</Button>
